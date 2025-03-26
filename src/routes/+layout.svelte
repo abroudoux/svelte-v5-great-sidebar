8907c9b6ddf518c452e99266d4121b3a2436b8a2
Arthur Broudoux <arthur.broudoux@ouest-france.fr>
2025-03-26 16:34:44 +0100

    feat: add header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,9 @@
     import "../app.css";
     import * as Sidebar from "$lib/components/ui/sidebar/index";
     import AppSidebar from "$lib/components/app-sidebar.svelte";
+    import * as Breadcrumb from "$lib/components/ui/breadcrumb";
+    import { Separator } from "$lib/components/ui/separator";
+    import ModeToggle from "$lib/components/mode-toggle.svelte";
 
     let { children } = $props();
 </script>
@@ -12,6 +15,22 @@
     <AppSidebar />
     <Sidebar.Inset>
         <!-- <Sidebar.Trigger class="m-2" /> -->
+        <header
+            class="bg-background sticky top-0 flex h-16 shrink-0 items-center justify-between gap-2 border-b px-4 rounded-t-xl flex-row"
+        >
+            <div class="flex flex-row items-center gap-1">
+                <Sidebar.Trigger class="-ml-1" />
+                <Separator orientation="vertical" class="mr-2 h-4" />
+                <Breadcrumb.Root>
+                    <Breadcrumb.List>
+                        <Breadcrumb.Item>
+                            <Breadcrumb.Page>Dashboard</Breadcrumb.Page>
+                        </Breadcrumb.Item>
+                    </Breadcrumb.List>
+                </Breadcrumb.Root>
+            </div>
+            <ModeToggle />
+        </header>
         <main class="p-4">
             {@render children?.()}
         </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,3 @@
-<script lang="ts">
-    import ModeToggle from "$lib/components/mode-toggle.svelte";
-</script>
-
 <div class="flex gap-3 flex-col">
     <h1>hello world</h1>
-    <ModeToggle />
 </div>
