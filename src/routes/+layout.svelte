90a11be5a94d60dd17545a852fa45f6ec0f6f17f
Arthur Broudoux <arthur.broudoux@ouest-france.fr>
2025-03-26 15:39:08 +0100

    feat: create the perfect layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,21 @@
 <script>
+    import { ModeWatcher } from "mode-watcher";
+
     import "../app.css";
+    import * as Sidebar from "$lib/components/ui/sidebar/index";
+    import AppSidebar from "$lib/components/AppSidebar.svelte";
+
+    let { children } = $props();
 </script>
 
-<slot />
+<Sidebar.Provider>
+    <AppSidebar />
+    <Sidebar.Inset>
+        <main class="p-4">
+            <!-- <Sidebar.Trigger /> -->
+            {@render children?.()}
+        </main>
+    </Sidebar.Inset>
+</Sidebar.Provider>
+
+<ModeWatcher />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-    import { Button } from "$lib/components/ui/button";
+    import ModeToggle from "$lib/components/ModeToggle.svelte";
 </script>
 
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
-<Button>Click me!</Button>
+<ModeToggle />
